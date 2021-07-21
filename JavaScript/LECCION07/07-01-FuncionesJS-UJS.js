ae41b15080ebf4c2f91daf6c10444710cc44968c
Melvin Paul Gonzalez Pascual <46496974+melvindragneel1@users.noreply.github.com>
2021-07-21 12:44:34 -0500

    Update 07-01-FuncionesJS-UJS.js
    Cambios del curso

--- JavaScript/LECCION07/07-01-FuncionesJS-UJS.js
+++ JavaScript/LECCION07/07-01-FuncionesJS-UJS.js
@@ -1,6 +1,23 @@
 //Declaracion de la funcion
 function miFuncion(a,b){
-    console.log("Suma: "+(a+b));
+    console.log(arguments.length);
+    return a + b;
 }
 
-miFuncion(2,3);
+//Llamando a la funcion
+let suma=miFuncion(2,3);
+console.log(suma)
+
+//Declaracion de una funcion de tipo expresio
+let x= function(a,b){return a + b};
+
+suma=x(1,2);
+console.log(suma);
+
+(function (a,b){
+    console.log('Ejecutando la funcion '+ (a+b));
+})(3,4);
+
+console.log(typeof miFuncion);
+
+var m
