810d892b6e13876867a51b7e6ef1b44e85fc21b9
melvindragneel1 <melvindragneel1@gmail.com>
2021-08-30 21:18:49 -0500

    Cambios del curso de Front, anexo archivos y modificaciones en JS

--- JavaScript/LECCION07/07-01-FuncionesJS-UJS.js
+++ JavaScript/LECCION07/07-01-FuncionesJS-UJS.js
@@ -1,16 +1,25 @@
 //Tipos primitivos
-let x=10;
+let x = 10;
 
-function cambiarValor(a){
-    a=20;
-
+function cambiarValor(a) {
+  a = 20;
 }
 cambiarValor(x);
 console.log(x);
 //console.log(a);
 
-const persona={
-    nombre: 'Juan',
-    apellido:'Perez'
+const persona = {
+  nombre: "Juan",
+  apellido: "Perez",
+};
+
+console.log(persona);
+
+function cambiarValorObjeto(p1) {
+    p1.nombre='Carlos';
+    p1.apellido='Lara';
 }
 
+//Paso por referencia
+cambiarValorObjeto(persona);
+console.log(persona);
